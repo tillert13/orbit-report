9a3856f4f78dbe4429b082454e4857a383b170de
Travion Tiller <tillert13@yahoo.com>
2019-10-28 11:35:36 -0500

    finished assignment

--- Launch-Checklist-Form-master/script.js
+++ Launch-Checklist-Form-master/script.js
@@ -6,17 +6,35 @@ window.addEventListener("load", function () {
       let copilotNameInput = document.querySelector("input[name=copilotName]");
       let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
       let cargoMassInput = document.querySelector("input[name=cargoMass]");
+      let updatedVisibilty = document.getElementById("faultyItems");
+      let updatedPilot = document.getElementById("pilotStats");
+      let updatedCopilot = document.getElementById("copilotStatus");
+      pilotStatus.innerText = `Pilot: ${pilotNameInput.value} is ready`
+      updatedCopilot.innerText = `Co-pilot: ${copilotNameInput.value} is ready`;
+      launchStatus.innerText = 'Shuttle is ready for launch';
+      launchStatus.style.color = 'green';
       if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
          alert("All fields are required!");
          event.preventDefault();
-      }
-      if (isNaN(pilotNameInput.value) === false || isNaN(copilotNameInput.value) === false || isNaN(fuelLevelInput.value) === true || isNaN(cargoMassInput.value) === true) {
+      } else if (isNaN(pilotNameInput.value) === false || isNaN(copilotNameInput.value) === false || isNaN(fuelLevelInput.value) === true || isNaN(cargoMassInput.value) === true) {
          alert("Please enter valid information!");
          event.preventDefault();
       }
-      
+      if (fuelLevelInput.value < 10000) {
+         updatedVisibilty.style.visibility = 'visible';
+         launchStatus.innerText = 'Shuttle not ready for launch';
+         launchStatus.style.color = 'red';
+         fuelStatus.innerText = 'Fuel level too low for launch';
+      }
+      if (cargoMassInput.value > 10000) {
+         updatedVisibilty.style.visibility = 'visible';
+         launchStatus.innerText = 'Shuttle not ready for launch';
+         launchStatus.style.color = 'red';
+         cargoStatus.innerText = 'Cargo mass too high for launch';
+      }
    });
 });
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
