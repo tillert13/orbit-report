8ed4895306a9d463444e34121c90977bee3f82d9
Travion Tiller <tillert13@yahoo.com>
2019-11-01 18:26:16 -0500

    defined and created sattelites

--- orbit-report/src/app/satellite.ts
+++ orbit-report/src/app/satellite.ts
@@ -1,2 +1,9 @@
 export class Satellite {
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) { }
+
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
 }
